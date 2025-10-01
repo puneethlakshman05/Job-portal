b4f85033f579aecfb2a8972f0a2e24ca8b8dc98d
puneethlakshman05 <puneethlaksh05gmail.com>
2025-10-02 00:18:55 +0530

    Fix Clerk webhooks raw body + logging

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 const connectDB = async() =>{
     mongoose.connection.on('connected',()=> console.log('Database connected'));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
 }
 
 export default connectDB
