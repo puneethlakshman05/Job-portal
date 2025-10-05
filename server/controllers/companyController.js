2c8347dd8c901ca56492ae86b4ecc1e1cc05deb1
puneethlakshman05 <puneethlaksh05gmail.com>
2025-10-05 15:21:18 +0530

    project completing

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,5 @@
 import Company from "../models/Company.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import {v2 as cloudinary} from "cloudinary";
 import generateToken from "../utils/generateToken.js";
 import Job from "../models/Job.js";
